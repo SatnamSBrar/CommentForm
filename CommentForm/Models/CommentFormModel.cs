77361621dea7406f289874fa7e83e0d9dd9bd33f
Satnam Brar <satnamsbrar@gmail.com>
2017-02-07 14:29:44 -0500

    added properties to CommentFormModel class

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommentForm.Models
 {
     public class CommentFormModel
     {
+        [Key]
+        public int ID { get; set; }
+        /* same as typing public int ID {
+         * get { return this.ID; }
+         * set {this.ID = value; }
+         * } */
+
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
 
     }
 }
